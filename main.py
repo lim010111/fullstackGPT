3cd2f7066a1764be1007062a3d0e908ff74e7dc2
WuhyunLi <shinedngus225@gmail.com>
2024-08-18 15:45:07 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from langchain.schema import HumanMessage, AIMessage, SystemMessage
+
+messages = [
+    SystemMessage(
+        content="You are a brain scientist. And you only reply in Korean",
+    )
+]
